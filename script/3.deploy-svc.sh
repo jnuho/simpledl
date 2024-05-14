5de9c03dd6609a566545bc78f12dc12cfc1e8b42
jnuho <cactoos555@gmail.com>
2024-05-14 15:43:52 +0900

    . ;

--- script/3.deploy-svc.sh
+++ script/3.deploy-svc.sh
@@ -1,10 +1,19 @@
 #!/bin/bash
 
+# fe-nginx
 microk8s kubectl apply -f deployment.yaml
-microk8s kubectl apply -f be/go/deployment.yaml
-microk8s kubectl apply -f be/py/deployment.yaml
 
+# be-go
 microk8s kubectl apply -f service.yaml
+microk8s kubectl apply -f be/go/deployment.yaml
 microk8s kubectl apply -f be/go/service.yaml
+
+# be-py
+microk8s kubectl apply -f be/py/deployment.yaml
 microk8s kubectl apply -f be/py/service.yaml
 
+# mongodb, mongo-express
+microk8s kubectl apply -f mongo/mongo-configmap.yaml
+microk8s kubectl apply -f mongo/mongo-secret.yaml
+microk8s kubectl apply -f mongo/mongodb.yaml
+microk8s kubectl apply -f mongo/mongo-express.yaml
--- script/delete.sh
+++ script/delete.sh
@@ -10,6 +10,12 @@ microk8s kubectl delete -f deployment.yaml
 microk8s kubectl delete -f be/go/deployment.yaml
 microk8s kubectl delete -f be/py/deployment.yaml
 
+# mongodb, mongo-express
+microk8s kubectl delete -f mongo/mongo-configmap.yaml
+microk8s kubectl delete -f mongo/mongo-secret.yaml
+microk8s kubectl delete -f mongo/mongodb.yaml
+microk8s kubectl delete -f mongo/mongo-express.yaml
+
 #microk8s kubectl delete secret regcred
 
 microk8s kubectl get pod
