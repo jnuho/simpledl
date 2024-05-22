b51fa546365524c4105c7d07b8ec5a8eece2cc2f
jnuho <cactoos555@gmail.com>
2024-05-22 22:17:50 +0900

    .

--- macro/test.go
+++ macro/test.go
@@ -1,17 +1,32 @@
 package main
 
 import (
-	"flag"
 	"fmt"
 )
 
-func print(o interface{}) {
-	fmt.Println(o)
+func print(o ...interface{}) {
+	fmt.Println(o...)
 }
 
 func main() {
-	stringFlag := flag.String("web-port", ":3001", "Enter a string and have it printed back out.")
-	flag.Parse()
+	fmt.Println('a')
+	fmt.Println("a")
+	slice1 := make([]int, 3, 5)
+	slice2 := append(slice1, 4, 5)
+	slice3 := append(slice2, 6)
 
-	fmt.Println("You entered:", *stringFlag)
+	// len() cap()
+	fmt.Println("slice1: ", slice1, len(slice1), cap(slice1))
+	fmt.Println("slice2: ", slice2, len(slice2), cap(slice2))
+	// 새로운 capacity 적용된 배열은 다른 주소가리킴!
+	fmt.Println("slice3: ", slice3, len(slice3), cap(slice3))
+
+	// 문제1: slice1변경 시 둘다 바뀜
+	slice1[1] = 100
+	fmt.Println("After slice1[1]=100")
+	fmt.Println("slice1: ", slice1, len(slice1), cap(slice1))
+	fmt.Println("slice2: ", slice2, len(slice2), cap(slice2))
+	// 새로운 capacity 적용된 배열은 다른 주소가리킴!
+	fmt.Println("slice3: ", slice3, len(slice3), cap(slice3))
+
 }
