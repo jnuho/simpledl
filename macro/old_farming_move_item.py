b17496c5275d55590938045d16caf38b8e0d6d82
jnuho <cactoos555@gmail.com>
2024-05-24 17:57:03 +0900

    .

--- macro/old_farming_move_item.py
+++ macro/old_farming_move_item.py
@@ -46,10 +46,11 @@ class GController(object):
     def handle_interrupt(signum, frame):
         controller.stop()
 
-    def get_food(self):
-        while self.running:
-            time.sleep(1)
-            print(datetime.datetime.now())
+
+    def cleanup(self):
+        # while self.running:
+        if self.running:
+            # time.sleep(24*60*60)
 
             windows = []
             title = base64.b64decode("R2Vyc2FuZw==").decode("utf-8")
@@ -70,39 +71,72 @@ class GController(object):
                 print(w)
                 time.sleep(.8)
 
+                # game_window.activate()
                 self.mouse_l_click(w.left + (w.width*.2049), w.top + (w.height*.4341))
                 self.pressAndRelease(Key.enter)
-                self.pressAndRelease(Key.esc)
                 self.pressAndRelease('i')
+                
+                # MOVE ITEMS
+                self.mouse_l_click(w.left + (w.width*.2029), w.top + (w.height*.5747))
+                self.mouse_l_click(w.left + (w.width*.5796), w.top + (w.height*.6261))
+                # self.mouse_l_click(w.left + (w.width*.7068), w.top + (w.height*.729))
+                self.pressAndRelease('5')
+                self.pressAndRelease('0')
+                self.pressAndRelease('0')
 
-                # Food
-                pag.moveTo(w.left + (w.width*.5835), w.top + (w.height*.2484))
-                time.sleep(.3)
 
-                self.mouse_r_click()
-                self.mouse_r_click()
-                self.mouse_r_click()
-                self.mouse_r_click()
+                self.pressAndRelease(Key.enter)
 
-                time.sleep(.3)
+                self.mouse_l_click(w.left + (w.width*.2417), w.top + (w.height*.5747))
+                self.mouse_l_click(w.left + (w.width*.5796), w.top + (w.height*.6261))
+                # self.mouse_l_click(w.left + (w.width*.7068), w.top + (w.height*.729))
+                self.pressAndRelease('5')
+                self.pressAndRelease('0')
+                self.pressAndRelease('0')
+                self.pressAndRelease(Key.enter)
+                
+                self.mouse_l_click(w.left + (w.width*.2845), w.top + (w.height*.5747))
+                self.mouse_l_click(w.left + (w.width*.5796), w.top + (w.height*.6261))
+                # self.mouse_l_click(w.left + (w.width*.7068), w.top + (w.height*.729))
+                self.pressAndRelease('5')
+                self.pressAndRelease('0')
+                self.pressAndRelease('0')
+                self.pressAndRelease(Key.enter)
+
+                # 아이템 삭제
                 self.pressAndRelease('j')
-                time.sleep(.3)
+                
+                self.mouse_l_click(w.left + (w.width*.8524), w.top + (w.height*.6826))
+                self.mouse_l_click(w.left + (w.width*.668), w.top + (w.height*.2472))
+                # self.mouse_l_click(w.left + (w.width*.799), w.top + (w.height*.3476))
+                self.pressAndRelease('5')
+                self.pressAndRelease('0')
+                self.pressAndRelease('0')
+                self.pressAndRelease(Key.enter)
+                self.pressAndRelease(Key.enter)
+
+                self.mouse_l_click(w.left + (w.width*.7097), w.top + (w.height*.2472))
+                # self.mouse_l_click(w.left + (w.width*.8417), w.top + (w.height*.3425))
+                self.pressAndRelease('5')
+                self.pressAndRelease('0')
+                self.pressAndRelease('0')
+                self.pressAndRelease(Key.enter)
+                self.pressAndRelease(Key.enter)
+
+                #다시시작
+                self.pressAndRelease('j')
                 self.mouse_l_click(w.left + (w.width*.2049), w.top + (w.height*.4341))
-                time.sleep(.3)
                 self.pressAndRelease('j')
-
-            time.sleep(25*60)
-
 
 
 if __name__ == "__main__":
     controller = GController()
 
     # Create two threads
-    food_thread = threading.Thread(target=controller.get_food)
+    cleanup_thread = threading.Thread(target=controller.cleanup)
 
     # Start both threads
-    food_thread.start()
+    cleanup_thread.start()
 
     # Wait for both threads to finish (which won't happen in this case)
-    food_thread.join()
+    cleanup_thread.join()
