0f990b95486ee4325a32fb1af2764d89704ffb2e
jnuho <cactoos555@gmail.com>
2024-05-27 17:12:48 +0900

    .

--- util/main.go
+++ util/main.go
@@ -4,11 +4,12 @@ import (
 	"fmt"
 
 	"github.com/jnuho/simpledl/pkg"
+	"github.com/jnuho/simpledl/util/crawl"
 )
 
 func main() {
 	found := pkg.BinarySearch(7, []int{1, 2, 5, 7, 23, 66, 99})
 	fmt.Println(found)
 
-	Crawl()
+	crawl.Crawl()
 }
