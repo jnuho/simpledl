e12e67722ae0dcb506104bf3daae20418b5bc34c
=jnuho <cactoos555@gmail.com>
2024-06-30 00:07:43 +0900

    tf update

--- script/terraform/6-eks.tf
+++ script/terraform/6-eks.tf
@@ -1,4 +1,3 @@
-
 # specifies which entities (users, services, or accounts) are allowed to assume the role.
 # In the context of this .tf, it allows the Amazon EKS service to assume the role;
 # attach the required Amazon EKS IAM managed policy to it.
@@ -14,19 +13,22 @@ data "aws_iam_policy_document" "cluster-role-assume-policy" {
   }
 }
 
-resource "aws_iam_role" "my-role" {
-  name = "my-cluster-role"
+# This role is assumed by the EKS control plane to manage the cluster.
+# : attach AmazonEKSClusterPolicy to this role
+
+resource "aws_iam_role" "eks_cluster_role" {
+  name = "eks-cluster-role"
   assume_role_policy = data.aws_iam_policy_document.cluster-role-assume-policy.json
 }
 
-resource "aws_iam_role_policy_attachment" "my-cluster-role-policy" {
-  role       = aws_iam_role.my-role.name
+resource "aws_iam_role_policy_attachment" "eks_cluster_role_policy" {
+  role       = aws_iam_role.eks_cluster_role.name
   policy_arn = "arn:aws:iam::aws:policy/AmazonEKSClusterPolicy"
 }
 
 resource "aws_eks_cluster" "my-cluster" {
   name     = "my-cluster"
-  role_arn = aws_iam_role.my-role.arn
+  role_arn = aws_iam_role.eks_cluster_role.arn
   version  = var.eks_version
 
   vpc_config {
@@ -38,13 +40,59 @@ resource "aws_eks_cluster" "my-cluster" {
     ]
   }
 
-  depends_on = [aws_iam_role_policy_attachment.my-cluster-role-policy]
+  depends_on = [aws_iam_role_policy_attachment.eks_cluster_role_policy]
+}
+
+# To use some Amazon EKS add-ons(vpc-cni), or to enable individual Kubernetes workloads
+# to have specific AWS Identity and Access Management (IAM) permissions,
+# create an IAM OpenID Connect (OIDC) provider for your cluster.
+# You only need to create an IAM OIDC provider for your cluster once.
+
+# If an add-on(vpc-cni) requires IAM permissions,
+# then you must have an IAM OpenID Connect (OIDC) provider for your cluster.
+
+# Create an OIDC provider for the EKS cluster
+data "aws_eks_cluster" "my-cluster" {
+  name = "my-cluster"
+}
+
+data "aws_eks_cluster_auth" "my-cluster" {
+  name = data.aws_eks_cluster.my-cluster.name
+}
+
+resource "aws_iam_openid_connect_provider" "oidc_provider" {
+  client_id_list  = ["sts.amazonaws.com"]
+  thumbprint_list = [data.aws_eks_cluster.my-cluster.certificate_authority[0].data]
+  url             = data.aws_eks_cluster.my-cluster.identity[0].oidc[0].issuer
 }
 
 
-// If an add-on requires IAM permissions,
-// then you must have an IAM OpenID Connect (OIDC) provider for your cluster. 
+# The vpc-cni plugin requires you to attach the following IAM policies to an IAM role:
+# AmazonEKS_CNI_Policy
 
+resource "aws_iam_policy_document" "vpc_cni_assume_role_policy" {
+  statement {
+    actions = ["sts:AssumeRole"]
+
+    principals {
+      type        = "Service"
+      identifiers = ["eks.amazonaws.com"]
+    }
+  }
+}
+
+# This role is specifically for the VPC CNI add-on, which handles networking for the EKS cluster.
+# : attach AmazonEKS_CNI_Policy to this role
+
+resource "aws_iam_role" "eks_cni_role" {
+  name = "eks-cni-role"
+  assume_role_policy = data.aws_iam_policy_document.vpc_cni_assume_role_policy.json
+}
+
+resource "aws_iam_role_policy_attachment" "eks_cni_role_policy" {
+  role       = aws_iam_role.eks_cni_role.name
+  policy_arn = "arn:aws:iam::aws:policy/AmazonEKS_CNI_Policy"
+}
 
 resource "aws_eks_addon" "addons" {
   for_each                = { for addon in var.addons : addon.name => addon }
--- script/terraform/7-node.tf
+++ script/terraform/7-node.tf
@@ -1,42 +1,68 @@
+data "aws_iam_policy_document" "nodes_assume_role_policy" {
+  statement {
+    actions = ["sts:AssumeRole"]
 
-resource "aws_iam_role" "nodes" {
-  name = "eks-node-group-nodes"
+    principals {
+      type        = "Service"
+      identifiers = ["ec2.amazonaws.com"]
+    }
 
-  assume_role_policy = jsonencode({
-    Statement = [{
-      Action = "sts:AssumeRole"
-      Effect = "Allow"
-      Principal = {
-        Service = "ec2.amazonaws.com"
-      }
-    }]
-    Version = "2012-10-17"
-  })
+    effect = "Allow"
+  }
+
+  version = "2012-10-17"
 }
 
-resource "aws_iam_role_policy_attachment" "nodes-AmazonEKSWorkerNodePolicy" {
+resource "aws_iam_role" "nodes" {
+  name               = "eks-node-group-nodes"
+  assume_role_policy = data.aws_iam_policy_document.nodes_assume_role_policy.json
+}
+
+resource "aws_iam_role_policy_attachment" "nodes_AmazonEKSWorkerNodePolicy" {
   policy_arn = "arn:aws:iam::aws:policy/AmazonEKSWorkerNodePolicy"
   role       = aws_iam_role.nodes.name
 }
 
-resource "aws_iam_role_policy_attachment" "nodes-AmazonEKS_CNI_Policy" {
+resource "aws_iam_role_policy_attachment" "nodes_AmazonEKS_CNI_Policy" {
   policy_arn = "arn:aws:iam::aws:policy/AmazonEKS_CNI_Policy"
   role       = aws_iam_role.nodes.name
 }
 
-resource "aws_iam_role_policy_attachment" "nodes-AmazonEC2ContainerRegistryReadOnly" {
+resource "aws_iam_role_policy_attachment" "nodes_AmazonEC2ContainerRegistryReadOnly" {
   policy_arn = "arn:aws:iam::aws:policy/AmazonEC2ContainerRegistryReadOnly"
   role       = aws_iam_role.nodes.name
 }
 
-resource "aws_eks_node_group" "private-nodes" {
+# Ensure that your nodes have the necessary security group rules to allow traffic.
+
+resource "aws_security_group" "eks_node_group_sg" {
+  name        = "eks-node-group-sg"
+  description = "Security group for EKS node group"
+  vpc_id      = aws_vpc.main.id
+
+  ingress {
+    from_port   = 0
+    to_port     = 65535
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 65535
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
+resource "aws_eks_node_group" "private_nodes" {
   cluster_name    = aws_eks_cluster.my-cluster.name
   node_group_name = "private-nodes"
   node_role_arn   = aws_iam_role.nodes.arn
 
   subnet_ids = [
-    aws_subnet.private-ap-northeast-2a.id,
-    aws_subnet.private-ap-northeast-2b.id
+    aws_subnet.private_ap_northeast_2a.id,
+    aws_subnet.private_ap_northeast_2b.id
   ]
 
   capacity_type  = "ON_DEMAND"
@@ -63,18 +89,18 @@ resource "aws_eks_node_group" "private-nodes" {
   # }
 
   # launch_template {
-  #   name    = aws_launch_template.eks-with-disks.name
-  #   version = aws_launch_template.eks-with-disks.latest_version
+  #   name    = aws_launch_template.eks_with_disks.name
+  #   version = aws_launch_template.eks_with_disks.latest_version
   # }
 
   depends_on = [
-    aws_iam_role_policy_attachment.nodes-AmazonEKSWorkerNodePolicy,
-    aws_iam_role_policy_attachment.nodes-AmazonEKS_CNI_Policy,
-    aws_iam_role_policy_attachment.nodes-AmazonEC2ContainerRegistryReadOnly,
+    aws_iam_role_policy_attachment.nodes_AmazonEKSWorkerNodePolicy,
+    aws_iam_role_policy_attachment.nodes_AmazonEKS_CNI_Policy,
+    aws_iam_role_policy_attachment.nodes_AmazonEC2ContainerRegistryReadOnly,
   ]
 }
 
-# resource "aws_launch_template" "eks-with-disks" {
+# resource "aws_launch_template" "eks_with_disks" {
 #   name = "eks-with-disks"
 
 #   key_name = "local-provisioner"
