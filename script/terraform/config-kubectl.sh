e8cbf7788f10ea0e0d15fee0ca5b518fa82e7f4e
=jnuho <cactoos555@gmail.com>
2024-07-01 23:01:14 +0900

    .

--- script/terraform/config-kubectl.sh
+++ script/terraform/config-kubectl.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-aws eks update-kubeconfig --region ap-northeast-2 --name my-cluster
+aws eks update-kubeconfig --region ap-northeast-2 --name my-cluster --profile terraform
 
 sleep 1
 
