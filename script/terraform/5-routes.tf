e4e46095020679b57cb47846336fdf0e06d97301
=jnuho <cactoos555@gmail.com>
2024-07-01 10:23:52 +0900

    .

--- script/terraform/5-routes.tf
+++ script/terraform/5-routes.tf
@@ -1,8 +1,26 @@
 
+# public route table
+resource "aws_route_table" "public_rtb" {
+  vpc_id = aws_vpc.main.id
+
+  route {
+    # the CIDR block of the route
+    cidr_block                 = "0.0.0.0/0"
+
+    # identifier of a VPC internet gateway or a virtual private gateway
+    gateway_id                 = aws_internet_gateway.main.id
+  }
+  
+  # a map of tags to assign to the resource
+  tags = {
+    Name = "public rtb"
+  }
+}
+
+
 /* Private route tables route traffic through NAT gateways,
  * while the public route table routes traffic through an Internet gateway.
  */
-
 
 // private route table 01
 resource "aws_route_table" "private_rtb_01" {
@@ -10,11 +28,13 @@ resource "aws_route_table" "private_rtb_01" {
 
   route {
     cidr_block                 = "0.0.0.0/0"
+
+    # identifier of a VPC NAT gateway
     nat_gateway_id             = aws_nat_gateway.nat_gw_01.id
   }
 
   tags = {
-    Name = "Private rtb 01"
+    Name = "private rtb 01"
   }
 }
 
@@ -24,56 +44,44 @@ resource "aws_route_table" "private_rtb_02" {
 
   route {
     cidr_block                 = "0.0.0.0/0"
+
+    # identifier of a VPC NAT gateway
     nat_gateway_id             = aws_nat_gateway.nat_gw_02.id
   }
 
   tags = {
-    Name = "Private rtb 02"
+    Name = "private rtb 02"
   }
-}
-
-// public route table
-resource "aws_route_table" "public_rtb" {
-  vpc_id = aws_vpc.main.id
-
-  route {
-    cidr_block                 = "0.0.0.0/0"
-    gateway_id                 = aws_internet_gateway.main.id
-  }
-  
-  tags = {
-    Name = "Public rtb"
-  }
-}
-
-//resource "aws_route" "private_rtb" {
-//  route_table_id         = aws_route_table.private_rtb.id
-//  destination_cidr_block = "0.0.0.0/0"
-//  nat_gateway_id         = aws_nat_gateway.nat_gw.id
-//}
-//resource "aws_route" "public_rtb" {
-//  route_table_id         = aws_route_table.public_rtb.id
-//  destination_cidr_block = "0.0.0.0/0"
-//  gateway_id             = aws_internet_gateway.main.id
-//}
-
-resource "aws_route_table_association" "private_rtb_asso_01" {
-  subnet_id      = aws_subnet.private_1.id
-  route_table_id = aws_route_table.private_rtb_01.id
-}
-
-resource "aws_route_table_association" "private_rtb_asso_02" {
-  subnet_id      = aws_subnet.private_2.id
-  route_table_id = aws_route_table.private_rtb_02.id
 }
 
 resource "aws_route_table_association" "public_rtb_asso_01" {
+  # subnet id to create an association
   subnet_id      = aws_subnet.public_1.id
+
+  # id of the routing table to associate with
   route_table_id = aws_route_table.public_rtb.id
 }
 
 resource "aws_route_table_association" "public_rtb_asso_02" {
+  # subnet id to create an association
   subnet_id      = aws_subnet.public_2.id
+
+  # id of the routing table to associate with
   route_table_id = aws_route_table.public_rtb.id
 }
 
+resource "aws_route_table_association" "private_rtb_asso_01" {
+  # subnet id to create an association
+  subnet_id      = aws_subnet.private_1.id
+
+  # id of the routing table to associate with
+  route_table_id = aws_route_table.private_rtb_01.id
+}
+
+resource "aws_route_table_association" "private_rtb_asso_02" {
+  # subnet id to create an association
+  subnet_id      = aws_subnet.private_2.id
+
+  # id of the routing table to associate with
+  route_table_id = aws_route_table.private_rtb_02.id
+}
