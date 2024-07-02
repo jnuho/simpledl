2653ebe6352bb9cf79d884faf0d7434a01021014
=jnuho <cactoos555@gmail.com>
2024-07-02 11:42:20 +0900

    .

--- script/terraform/13-aws-lbc.tf
+++ script/terraform/13-aws-lbc.tf
@@ -1,11 +1,9 @@
-
-data "aws_iam_policy_document" "aws-lbc-assume-policy" {
+data "aws_iam_policy_document" "aws_lbc" {
   statement {
     effect = "Allow"
 
     principals {
       type        = "Service"
-      # for IAM role to be used only by EKS services
       identifiers = ["pods.eks.amazonaws.com"]
     }
 
@@ -15,36 +13,36 @@ data "aws_iam_policy_document" "aws-lbc-assume-policy" {
     ]
   }
 }
-# This role is assumed by the EKS control plane to manage the cluster.
-resource "aws_iam_role" "aws-lbc" {
-  name = "aws-lbc"
-  assume_role_policy = data.aws_iam_policy_document.aws-lbc-assume-policy.json
+
+resource "aws_iam_role" "aws_lbc" {
+  name               = "${aws_eks_cluster.my-cluster.name}-aws-lbc"
+  assume_role_policy = data.aws_iam_policy_document.aws_lbc.json
 }
 
-resource "aws_iam_policy" "aws-lbc" {
+resource "aws_iam_policy" "aws_lbc" {
   policy = file("./iam/AWSLoadBalancerController.json")
-  name   = "AWSLoadBalancerControllerPolicy"
+  name   = "AWSLoadBalancerController"
 }
 
-resource "aws_iam_role_policy_attachment" "aws-lbc" {
-  role       = aws_iam_role.aws-lbc.name
-  policy_arn = aws_iam_policy.aws-lbc.arn
+resource "aws_iam_role_policy_attachment" "aws_lbc" {
+  policy_arn = aws_iam_policy.aws_lbc.arn
+  role       = aws_iam_role.aws_lbc.name
 }
 
-resource "aws_eks_pod_identity_association" "aws-lbc" {
-  cluster_name = aws_eks_cluster.my-cluster.name
-  namespace = "kube-system"
+resource "aws_eks_pod_identity_association" "aws_lbc" {
+  cluster_name    = aws_eks_cluster.my-cluster.name
+  namespace       = "kube-system"
   service_account = "aws-load-balancer-controller"
-  role_arn = aws_iam_role.aws-lbc.arn
+  role_arn        = aws_iam_role.aws_lbc.arn
 }
 
-resource "helm_release" "aws-lbc" {
+resource "helm_release" "aws_lbc" {
   name = "aws-load-balancer-controller"
 
   repository = "https://aws.github.io/eks-charts"
-  chart       = "aws-load-balancer-controller"
-  namespace   = "kube-system"
-  version     = "1.7.2"
+  chart      = "aws-load-balancer-controller"
+  namespace  = "kube-system"
+  version    = "1.7.2"
 
   set {
     name  = "clusterName"
@@ -56,7 +54,7 @@ resource "helm_release" "aws-lbc" {
     value = "aws-load-balancer-controller"
   }
 
-  # depends_on = [ aws_eks_node_group.private_nodes ]
   # depends_on = [helm_release.cluster_autoscaler]
+  # depends_on = [ aws_eks_node_group.private_nodes ]
   depends_on = [ helm_release.metric_server]
 }
